642ca8a8ffce434ee01a698829430750b2d04950
pxjuns <pjuns.code@gmail.com>
2026-01-31 00:37:53 +0100

    feat: adding uniwind

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -1,19 +1,17 @@
-import Animated from 'react-native-reanimated';
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import React from "react";
 
-export function HelloWave() {
+export const HelloWave = () => {
+  const width = 256;
+  const height = 256;
+  const r = width * 0.33;
   return (
-    <Animated.Text
-      style={{
-        fontSize: 28,
-        lineHeight: 32,
-        marginTop: -6,
-        animationName: {
-          '50%': { transform: [{ rotate: '25deg' }] },
-        },
-        animationIterationCount: 4,
-        animationDuration: '300ms',
-      }}>
-      👋
-    </Animated.Text>
+    <Canvas style={{ width, height }}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={width - r} cy={r} r={r} color="magenta" />
+        <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+      </Group>
+    </Canvas>
   );
-}
+};
