73e02e8c89981b259cd0a598514397909e7934b7
pxjuns <pjuns.code@gmail.com>
2026-01-31 00:43:27 +0100

    feat: adding tailwind utils

---
+++ common/utils/tw.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { createTV } from "tailwind-variants";
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const twMergeConfig = {};
+
+export const twVariants = createTV({});
